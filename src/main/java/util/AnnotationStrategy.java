63ade4cfd5ebcfd0c220bb1991a19a497c8c3689
Bobby <bodavis@csumb.edu>
2022-01-28 03:34:30 -0800

    Implemented creatTable method

--- src/main/java/util/AnnotationStrategy.java
+++ src/main/java/util/AnnotationStrategy.java
@@ -1,9 +1,71 @@
 package util;
 
+import annotations.Entity;
+import annotations.Id;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
+
+//TODO
+//change to string builders
+//remove generics
+
 public class AnnotationStrategy<T> implements MappingStrategy <T> {
     @Override
     public String createTable(Class clazz){
-        String query = null;
+        String query = "CREATE TABLE ";
+
+        Object tableName = null;
+        try{
+            Annotation annotation = clazz.getAnnotation(Entity.class);
+            Method m = annotation.annotationType().getMethod("name");
+            tableName = m.invoke(annotation);
+        } catch(NoSuchMethodException | java.lang.IllegalAccessException | java.lang.reflect.InvocationTargetException e){
+            System.out.println(e.getMessage());
+        }
+
+        String name = !tableName.equals("none") ? tableName + "" : clazz.getSimpleName();
+        query += (name + " (\n");
+
+        Properties props = new Properties();
+        try{
+            props.load(AnnotationStrategy.class.getClassLoader().getResourceAsStream("mapping.properties"));
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        boolean hasPk = false;
+        String pkName = "";
+        int count = 1;
+        Field[] fields = clazz.getDeclaredFields();
+        for(Field f : fields){
+            String fieldName = f.getName();
+            query += "  " + fieldName + " ";
+            if(f.isAnnotationPresent(Id.class)){
+                query += "serial";
+                hasPk = true;
+                pkName = fieldName;
+            }else{
+                query += (props.getProperty(f.getType().getName()));
+            }
+            if(!(count >= fields.length)){
+                query += ",\n";
+            }
+            count++;
+        }
+        if(hasPk){
+            query += ",\n  primary key (" + pkName + ")";
+        }
+
+        query += "\n);";
+
         return query;
     }
     @Override
