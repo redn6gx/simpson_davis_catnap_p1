68f650ad2472dc081a2e2f4f78c869478b4d38c1
Bobby <bodavis@csumb.edu>
2022-01-28 00:52:53 -0800

    Added AnnotationStrategy class with method skeletons

---
+++ src/main/java/util/AnnotationStrategy.java
@@ -0,0 +1,39 @@
+package util;
+
+public class AnnotationStrategy<T> implements MappingStrategy <T> {
+    @Override
+    public String createTable(Class clazz){
+        String query = null;
+        return query;
+    }
+    @Override
+    public String dropTable(Class clazz){
+        String query = null;
+        return query;
+    }
+    @Override
+    public String insert(Class clazz){
+        String query = null;
+         return query;
+    }
+    @Override
+    public String get(Class clazz){
+        String query = null;
+        return query;
+    }
+    @Override
+    public String getAll(Class clazz){
+        String query = null;
+        return query;
+    }
+    @Override
+    public String update(Class clazz){
+        String query = null;
+        return query;
+    }
+    @Override
+    public String delete(Class clazz){
+        String query = null;
+        return query;
+    }
+}
