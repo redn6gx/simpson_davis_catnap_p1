e08ef9ef21c334831ad104992c3600b6a8811486
Richard Simpson <richard.simpson235@gmail.com>
2022-01-29 18:04:18 -0500

    Add main exception type for Catnap

---
+++ src/main/java/exceptions/CatnapException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class CatnapException extends Exception {
+
+    public CatnapException() {
+    }
+
+    public CatnapException(String message) {
+        super(message);
+    }
+}
