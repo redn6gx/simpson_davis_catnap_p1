edbc9f32effefb373d8efd3535b69cfdc1045cfe
Richard Simpson <richard.simpson235@gmail.com>
2022-01-31 16:52:26 -0500

    Remove association relevant methods and information

--- src/main/java/util/CatnapResult.java
+++ src/main/java/util/CatnapResult.java
@@ -1,8 +1,6 @@
 package util;
 
 import annotations.Id;
-import annotations.OneToMany;
-import annotations.OneToOne;
 import exceptions.CatnapException;
 
 import java.lang.reflect.Field;
@@ -18,11 +16,9 @@ import java.util.stream.Collectors;
 public class CatnapResult {
 
     private final Object entity;
-    private final Map<String, Integer> foreignKeys;
 
     public CatnapResult(Object entity) {
         this.entity = entity;
-        this.foreignKeys = new HashMap<>();
     }
 
     public Object getEntity() {
@@ -31,21 +27,12 @@ public class CatnapResult {
 
     public Class<?> getEntityType() { return entity.getClass(); }
 
-    public Optional<Integer> getForeignKey(String fieldName) {
-        return Optional.of(this.foreignKeys.get(fieldName));
-    }
-
-    public void addForeignKey(String fieldName, int value) {
-        this.foreignKeys.put(fieldName, value);
-    }
-
     /**
      * This method returns an Optional containing the id of the entity if it finds the id field.
      * @return                    optional containing the id of the entity
      * @throws CatnapException    thrown when the field was inaccessible
      */
     public Optional<Integer> getId() throws CatnapException {
-        // may refactor getIdField into this method if it's never used
         Optional<Field> idField = getIdField();
         if(idField.isPresent()) {
             idField.get().setAccessible(true);
@@ -72,56 +59,11 @@ public class CatnapResult {
     }
 
     /**
-     * Filters through the fields of the entity and returns ones that are not annotated with
-     * one of the assication annotations: OneToOne or OneToMany
+     * returns a list of declared fields
      *
-     * @return               a list of fields that are not marked with OneToOne or OneToMany
+     * @return               a list of the declared fields
      */
-    public List<Field> getNonAssociativeFields() {
-        return Arrays.stream(this.entity.getClass().getDeclaredFields())
-                .filter(field -> !(field.isAnnotationPresent(OneToOne.class) || field.isAnnotationPresent(OneToMany.class)))
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Checks to see if the entity has a one to one relationship defined
-     *
-     * @return                whether the entity has a one to one relationship defined
-     */
-    public boolean hasOneToOneField() {
-        return Arrays.stream(this.entity.getClass().getDeclaredFields())
-                .anyMatch(e -> e.isAnnotationPresent(OneToOne.class));
-    }
-
-    /**
-     * Checks to see if the entity has a one to many relationship defined
-     *
-     * @return                whether the entity has a one to many relationship defined
-     */
-    public boolean hasOneToManyField() {
-        return Arrays.stream(this.entity.getClass().getDeclaredFields())
-                .anyMatch(e -> e.isAnnotationPresent(OneToMany.class));
-    }
-
-    /**
-     * Retrieves a list of fields with the OneToOne annotation
-     *
-     * @return                list of fields with the OneToOne annotation
-     */
-    public List<Field> getOneToOneFields() {
-        return Arrays.stream(this.entity.getClass().getDeclaredFields())
-                .filter(e -> e.isAnnotationPresent(OneToOne.class))
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Retrieves a list of fields with the OneToMany annotation
-     *
-     * @return                list of fields with the OneToOne annotation
-     */
-    public List<Field> getOneToManyFields() {
-        return Arrays.stream(this.entity.getClass().getDeclaredFields())
-                .filter(e -> e.isAnnotationPresent(OneToMany.class))
-                .collect(Collectors.toList());
+    public List<Field> getFields() {
+        return Arrays.stream(this.entity.getClass().getDeclaredFields()).collect(Collectors.toList());
     }
 }
