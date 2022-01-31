08d2ea96d7db1683ffc5e8de7ec52fcc04066ae6
Bobby <bodavis@csumb.edu>
2022-01-31 12:41:20 -0800

    Removed relationship annotations

--- src/main/java/annotations/Entity.java
+++ src/main/java/annotations/Entity.java
@@ -6,5 +6,4 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Entity {
     String name() default "none";
-        //in annotation strategy, if "none".equals()
 }
