fe88e57f684424d4600c6e9335ce5625e9610438
Richard Simpson <richard.simpson235@gmail.com>
2022-02-01 12:49:38 -0500

    Change cache to make use of CatnapResult object

--- src/main/java/util/Cache.java
+++ src/main/java/util/Cache.java
@@ -1,33 +1,47 @@
 package util;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface Cache<T> {
 
     /**
-     * This method returns an optional of an entity of type T. If the
-     * entity is not in the cache the isPresent method of the optional
-     * will return false.
+     * This method is used to check if an entity is in the cache.
      *
-     * @param id database id of the entity
-     * @return   the java object representation of the entity
+     * @param clazz             the type of entity
+     * @param id                the id of the entity
+     * @return                  whether the entity has been cached
      */
-    public Optional<T> get(int id);
+    public boolean contains(Class<?> clazz, int id);
+
+    /**
+     * This method is used to get an entity from the cache.
+     *
+     * @param clazz           the type of entity to get
+     * @param id              the primary key of the entity
+     * @return                an optional containing the entity if it's found
+     */
+    public Optional<T> get(Class<?> clazz, int id);
 
     /**
      * This method caches the object for later retrieval.
      *
-     * @param id the id of the entity
      * @param t  the entity to store in the cache
      */
-    public void store(int id, T t);
+    public void store(T t);
 
     /**
-     * This methods is used to check if an entity is in the cache.
-     * Uses the entity's database id to do so.
+     * This method stores a collection of entities into the cache.
      *
-     * @param id the id of the entity
-     * @return   whether the entity has been cached
+     * @param entities        a collection of entities to store
      */
-    public boolean contains(int id);
+    public void store(Collection<T> entities);
+
+    /**
+     * This method removes an entity from the cache.
+     *
+     * @param entity          the entity to remove
+     */
+    public void remove(CatnapResult entity);
+
 }
